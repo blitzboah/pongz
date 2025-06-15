aee483b0bad577370b0813629e6e66c5b102483b
blitzboah <blitzboah@users.noreply.github.com>
2025-06-15 17:19:23 +0530

    pongzzzz

--- src/main.zig
+++ src/main.zig
@@ -3,20 +3,23 @@ const c = @cImport({
     @cInclude("raylib.h");
 });
 
-const Ball = struct {
+const Circle = struct {
     pos: c.Vector2,
     vel: c.Vector2,
 
-    pub fn update(self: *Ball, dt: f32) void{
-        const gravity = 10;
+    pub fn init(posX: f32, posY: f32, velX: f32, velY: f32) Circle {
+       return Circle {
+           .pos = c.Vector2{.x = posX, .y = posY},
+           .vel = c.Vector2{.x = velX, .y = velY},
+       }; 
+    }
 
-        self.vel.y += dt * gravity;
-
+    pub fn update(self: *Circle, dt: f32) void{
         self.pos.x += self.vel.x * dt;
         self.pos.y += self.vel.y * dt;
     }
 
-    pub fn draw(self: Ball, r: f32, color: c.Color) void {
+    pub fn draw(self: Circle, r: f32, color: c.Color) void {
         c.DrawCircleV(self.pos, r, color);
     }
 };
@@ -24,66 +27,113 @@ const Ball = struct {
 const Rectangle = struct {
     pos: c.Vector2,
     vel: c.Vector2,
-    down: bool = true,
 
-    pub fn update(self: *Rectangle, dt: f32) void {
-        if (self.down) {
-            self.pos.y += self.vel.y * dt;
-            if (self.pos.y >= 300.0){
-                self.down = false;
-            }
-        }
-        else {
-            self.pos.y -= self.vel.y * dt;
-            if (self.pos.y <= 100.0){
-                self.down = true;
-            }
-        }
+    pub fn init(posX: f32, posY: f32, velX: f32, velY: f32) Rectangle {
+       return Rectangle {
+           .pos = c.Vector2{.x = posX, .y = posY},
+           .vel = c.Vector2{.x = velX, .y = velY},
+       }; 
     }
+
+    
+    pub fn update(self: *Rectangle, ball: *Circle) void {
+        const paddle_half_height = 45.0;
+        const targetY = ball.pos.y - paddle_half_height;
+        self.pos.y = std.math.clamp(targetY, 10.0, 720.0 - 90.0 - 10.0);
+    }
+
 
     pub fn draw(self: Rectangle, size: c.Vector2, color: c.Color) void {
         c.DrawRectangleV(self.pos, size, color);
     }
 };
 
+fn checkCircleRectCollision(ballPos: c.Vector2, radius: f32, rectPos: c.Vector2, rectSize: c.Vector2) bool {
+    const clamp = std.math.clamp;
+
+    const cX = clamp(ballPos.x, rectPos.x, rectPos.x + rectSize.x);
+    const cY = clamp(ballPos.y, rectPos.y, rectPos.y + rectSize.y);
+
+    const dx = ballPos.x - cX;
+    const dy = ballPos.y - cY;
+
+    return (dx * dx + dy * dy) < (radius * radius);
+}
+
+fn UpdatePlayer(player: *Rectangle, dt: f32, rectSize: c.Vector2) void {
+    const speed : f32 = 200.0;
+    var pos = player.pos;
+
+    if(c.IsKeyDown(c.KEY_UP)) {
+        pos.y -= speed * dt;
+    }
+    if(c.IsKeyDown(c.KEY_DOWN)) {
+        pos.y += speed * dt;
+    }
+
+    player.pos = pos;
+    player.pos.y = std.math.clamp(player.pos.y, 10.0, 720.0 - rectSize.y - 10.0);
+}
+
 pub fn main() void {
 
     c.InitWindow(1280, 720, "window");
     c.SetTargetFPS(60);
+    const fixed_dt = 1.0 / 60.0;
 
     defer c.CloseWindow();
 
-    var ball = Ball{
-        .pos = c.Vector2{.x = 100.0, .y = 100.0},
-        .vel = c.Vector2{.x = 50.0, .y = 0.0},
-    };
+    var ball = Circle.init(300, 200, 300, 300);
+    var leftPaddle = Rectangle.init(100, 400, 0, 300);
+    var playerPaddle = Rectangle.init(1180, 400, 0, 100);
 
-    var rect1 = Rectangle{
-        .pos = c.Vector2{.x = 100.0, .y = 100.0},
-        .vel = c.Vector2{.x = 0.0, .y = 100.0},
-    };
-
-    var rect2 = Rectangle{
-        .pos = c.Vector2{.x = 500.0, .y = 200.0},
-        .vel = c.Vector2{.x = 0.0, .y = 100.0},
-    };
+    const radius: f32 = 10.0;
+    const rectSize = c.Vector2{ .x = 20, .y = 90 };
+    
+    var accumulator : f32 = 0.0;
 
     while (!c.WindowShouldClose()) {
-
         const dt = c.GetFrameTime();
+        accumulator += dt;
+        
+        UpdatePlayer(&playerPaddle, dt, rectSize);
+        //fixed update
+        while (accumulator >= fixed_dt){
+            ball.update(dt);
+            leftPaddle.update(&ball);
 
-        ball.update(dt);
-        rect1.update(dt);
-        rect2.update(dt);
+            if (checkCircleRectCollision(ball.pos, radius, leftPaddle.pos, rectSize)) {
+                ball.vel.x *= -1;
+            }
+            if (checkCircleRectCollision(ball.pos, radius, playerPaddle.pos, rectSize)) {
+                ball.vel.x *= -1;
+            }
+            if (ball.pos.y - radius <= 10 or ball.pos.y + radius >= 710) {
+                ball.vel.y *= -1;
+            }
+            if (ball.pos.x - radius <= 10 or ball.pos.x + radius >= 1270) {
+                ball.vel.x *= -1;
+            }
 
+            accumulator -= fixed_dt;
+        }
+
+        //drawing 
         c.BeginDrawing();
         defer c.EndDrawing();
 
         c.ClearBackground(c.BLACK);
         
-        c.DrawText("pongz", 0, 0, 20, c.WHITE);
-        ball.draw(10, c.SKYBLUE);
-        rect1.draw(c.Vector2{.x = 20, .y = 90}, c.WHITE);
-        rect2.draw(c.Vector2{.x = 20, .y = 90}, c.WHITE);
+        c.DrawText("pongz", 20, 20, 20, c.WHITE);
+
+        //walls
+        c.DrawRectangle(0, 0, 1280, 10, c.GRAY);   // top
+        c.DrawRectangle(0, 710, 1280, 10, c.GRAY); // bottom
+        c.DrawRectangle(0, 0, 10, 720, c.GRAY);    // left
+        c.DrawRectangle(1270, 0, 10, 720, c.GRAY); // right
+                                                         
+        ball.draw(radius, c.SKYBLUE);
+        leftPaddle.draw(rectSize, c.WHITE);
+        playerPaddle.draw(rectSize, c.WHITE);
     }
 }
